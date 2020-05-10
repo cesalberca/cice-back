1058d29075eed1ebad2cf2057592f1c445090cdb
César Alberca <cesalberca@gmail.com>
2020-05-10 21:39:31 +0200

    Add mongodb

---
+++ README.md
@@ -0,0 +1,5 @@
+db.users.insertMany([
+   { firstname: "Grace", lastname: "Hopper" },
+   { firstname: "Barbara", lastname: "Liskov" },
+   { firstname: "Ada", lastname: "Lovelace" }
+]);
--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
 const port = process.env.PORT || 8080
 
 const corsOptions = {
   origin: '*'
 }
 
-app.use(cors(corsOptions))
-app.get('/api', (req, res) => res.json({ message: 'hello world' }))
+let client
+let database
 
-app.listen(port)
+app.use(cors(corsOptions))
+app.get('/api', async (req, res) => {
+  const users = database.collection('users')
+
+  return res.json(await users.find({}).toArray())
+})
+
+app.listen(port, () => {
+  MongoClient.connect(
+    'mongodb://localhost:27017/cice-back',
+    { useUnifiedTopology: true },
+    (error, mongoClient) => {
+      if (error) {
+        throw error
+      }
+      client = mongoClient
+      database = client.db('cice-back')
+    }
+  )
+})
